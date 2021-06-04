e8f08ed0316c96da4fa5473d4f9b435fd294e0e7
tajinder-singh <tajinder.singh@outlook.in>
2021-06-04 23:07:58 +0530

    updates test cases

--- src/test/java/com/spr/sprart/SprArtApplicationTests.java
+++ src/test/java/com/spr/sprart/SprArtApplicationTests.java
@@ -26,7 +26,6 @@ class SprArtApplicationTests {
     public void testNumCompare() throws Throwable {
 
         TemperatureController underTest = new TemperatureController();
-        //assertEquals(3, underTest.CompareNumbers(3, 5));
         assertEquals(7, underTest.CompareNumbers(3, 7), "Test failed...");
 
     }
